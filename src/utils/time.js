3d206f2f907952d13b7f75acd97450a25122479d
Max DappVibe <max@dappvibe.com>
2025-07-13 17:20:33 +0500

    remove unused formatDateInTimezone method from time utils

--- src/utils/time.js
+++ src/utils/time.js
@@ -1,8 +1,4 @@
 /**
- * Time utility functions
- */
-
-/**
  * Get the current time formatted in a specific timezone
  * @param {string} tz - Timezone (e.g., 'Asia/Phnom_Penh', 'UTC', 'America/New_York')
  * @param {Object} options - Formatting options for toLocaleTimeString
@@ -12,18 +8,6 @@ function getTimeInTimezone(tz, options = { hour12: false }) {
     return new Date().toLocaleTimeString('en-US', { timeZone: tz, ...options });
 }
 
-/**
- * Format a date in a specific timezone
- * @param {Date} date - Date object to format
- * @param {string} tz - Timezone
- * @param {Object} options - Formatting options
- * @returns {string} - Formatted date string
- */
-function formatDateInTimezone(date, tz, options = {}) {
-    return date.toLocaleString('en-US', { timeZone: tz, ...options });
-}
-
 module.exports = {
-    getTimeInTimezone,
-    formatDateInTimezone,
+    getTimeInTimezone
 };
