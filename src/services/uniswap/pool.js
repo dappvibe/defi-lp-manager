fd59a5745d2e1e4dd5ea477441116148f9d046a3
Max DappVibe <max@dappvibe.com>
2025-11-11 16:42:23 +0800

    drop old pool code that was moved to Model

--- src/services/uniswap/pool.js
+++ src/services/uniswap/pool.js
@@ -21,64 +21,6 @@ class Pool extends EventEmitter {
   }
 
   /**
-   * Start monitoring this pool
-   */
-  async startMonitoring() {
-    if (this.isMonitoring) return;
-
-    await this.getPoolInfo();
-
-    const price = await this.getPrice();
-    if (!price) {
-      throw new Error('Failed to get current price for monitoring');
-    }
-
-    // Initialize monitoring data
-    this.monitoringData = {
-      lastPriceT1T0: price,
-      notifications: []
-    };
-
-    // Start listening for swap events
-    await this._attachSwapListener();
-    this.isMonitoring = true;
-
-    console.log(`Started monitoring pool ${this.info.token0.symbol}/${this.info.token1.symbol} (${this.info.fee}%)`);
-
-    // Save monitoring state
-    await this._saveMonitoringState();
-
-    return {
-      info: this.info,
-      price
-    };
-  }
-
-  /**
-   * Stop monitoring this pool
-   */
-  async stopMonitoring() {
-    if (!this.isMonitoring) {
-      return;
-    }
-
-    if (this.watchUnsubscriber) {
-      this.watchUnsubscriber();
-      this.watchUnsubscriber = null;
-    }
-
-    this.isMonitoring = false;
-    this.monitoringData = null;
-
-    // Update monitoring state in database
-    await this.model.savePoolState(this.address, {
-      priceMonitoringEnabled: false,
-      updatedAt: new Date()
-    });
-    console.log(`Stopped monitoring pool ${this.info.token0.symbol}/${this.info.token1.symbol} (${this.info.fee}%)`);
-  }
-
-  /**
    * Add price alert for this pool
    * @param {number} targetPrice - Target price for alert
    * @param {number} originalChatId - Original chat ID for notification
@@ -133,60 +75,7 @@ class Pool extends EventEmitter {
    * Attach swap event listener
    * @private
    */
-  async _attachSwapListener() {
-    this.watchUnsubscriber = await this.provider.watchContractEvent({
-      address: this.address,
-      contract: poolAbi,
-      eventName: 'Swap',
-      onLogs: (logs) => {
-        logs.forEach((log) => {
-          if (!this.monitoringData || !this.info) {
-            console.warn(`Pool info incomplete for ${this.address} during Swap event. Skipping.`);
-            return;
-          }
 
-          const { args } = log;
-          const { sqrtPriceX96, amount0, amount1, tick } = args;
-
-          const newPriceT1T0 = parseFloat(calculatePrice(sqrtPriceX96, this.info.token0.decimals, this.info.token1.decimals));
-
-          // Prepare swap information
-          const swapInfo = {
-            address: this.address,
-            transactionHash: log.transactionHash,
-            blockNumber: log.blockNumber,
-            logIndex: log.logIndex,
-            sender: args.sender,
-            recipient: args.recipient,
-            amount0: args.amount0,
-            amount1: args.amount1,
-            sqrtPriceX96: args.sqrtPriceX96,
-            liquidity: args.liquidity,
-            tick: args.tick,
-            newPrice: newPriceT1T0,
-            timestamp: getTimeInTimezone(this.timezone)
-          };
-
-          // Prepare pool information
-          const poolData = {
-            address: this.address,
-            token0: this.info.token0,
-            token1: this.info.token1,
-            fee: this.info.fee,
-            platform: this.info.platform,
-            blockchain: this.info.blockchain,
-            currentPrice: newPriceT1T0,
-            lastPrice: this.monitoringData.lastPriceT1T0
-          };
-
-          // Emit swap event
-          this.emit('swap', swapInfo, poolData);
-
-          this.monitoringData.lastPriceT1T0 = newPriceT1T0;
-        });
-      }
-    });
-  }
 
   /**
    * Save monitoring state to database
@@ -212,6 +101,5 @@ class Pool extends EventEmitter {
 }
 
 module.exports = (container) => {
-  container.register({
-  })
+
 };
