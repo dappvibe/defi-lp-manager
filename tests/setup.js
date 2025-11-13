091304a235eaa30189115b2943d67fdac567b171
Max DappVibe <max@dappvibe.com>
2025-11-14 07:25:27 +0800

    tests: mock viem client to ensure offline runs

--- tests/setup.js
+++ tests/setup.js
@@ -12,6 +12,7 @@
 const res = require('dotenv').config({path: '.env.example'});
 
 import { asClass, asValue } from "awilix";
+import { createPublicClient, custom } from "viem";
 import App from '../src/app'; // envvars are loaded here
 import * as mocks from './_mocks/contracts';
 
@@ -33,17 +34,17 @@ beforeAll(async () => {
     } else throw e;
   });
 
-  const erc20Factory = new mocks.MockERC20Factory();
-  const poolV3Factory = new mocks.MockPoolV3Factory(erc20Factory);
-  const poolContractFactory = new mocks.MockPoolContractFactory(poolV3Factory);
-  container.register({
-    chainId: asValue(42161), // Arbitrum
-    erc20Factory: asValue(erc20Factory.get.bind(erc20Factory)),
-    poolFactoryContract: asValue(poolV3Factory),
-    poolContract: asValue(poolContractFactory.get.bind(poolContractFactory)),
-    positionManager: asValue(new mocks.MockNonfungiblePositionManager()),
-    staker: asValue(new mocks.MockStaker)
-  });
+  // ensure no blockchain requests goes out
+  container.register('provider', asValue(createPublicClient({
+    chain: 'arbitrum',
+    transport: custom({
+      request: vi.fn(async ({ method, params }) => {
+        if (method === 'eth_blockNumber') {
+          return '0x123'; // Mocked block number (300)
+        }
+      })
+    })
+  })))
 });
 
 beforeEach(() => {
